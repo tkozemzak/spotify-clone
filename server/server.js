30b921ca488644d9c244549fff01f47c0a0729e7
tkozemzak <timkozemzak@gmail.com>
2021-03-19 17:44:32 -0700

    fix redirect

--- server/server.js
+++ server/server.js
@@ -1,22 +1,40 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const SpotifyWebApi = require("spotify-web-api-node");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT;
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
 
-app.listen(PORT, () => {
-  console.log("Listening On ", PORT);
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
 });
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
-
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
     clientId: process.env.CLIENT_ID,
@@ -27,36 +45,16 @@ app.post("/login", (req, res) => {
     .authorizationCodeGrant(code)
     .then((data) => {
       res.json({
-        accessToken: data.body.accessToken,
+        accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in,
       });
     })
     .catch((err) => {
-      console.log("error:"), err;
       res.sendStatus(400);
     });
 });
 
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
-    clientId: "f60fb94836aa403995129fe4340d673f",
-    clientSecret: "37cbdc55984743a083aa507f6b28f2d2",
-    refreshToken,
-  });
-
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.json({
-        accessToken: data.body.access_token,
-        expiresIn: data.body.expires_in,
-      });
-      spotifyApi.setAccessToken(data.body["access_token"]);
-    })
-    .catch(() => {
-      res.sendStatus(400);
-    });
+app.listen(PORT, () => {
+  console.log("Listening on: ", PORT);
 });
