4c7c858d4b15553908af9898ef516a5a14a46c8e
tkozemzak <timkozemzak@gmail.com>
2021-04-12 18:21:12 -0700

    fix clientid in client

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,10 +1,6 @@
-import { useContext } from "react";
 import { Container } from "react-bootstrap";
-import { Context } from "../Contexts/Store";
 
-export default function Login({ clientId }) {
-  const [state] = useContext(Context);
-
+export default function Login() {
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=b2a4c85d08204823ad65906e7a6a3207&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
   return (
